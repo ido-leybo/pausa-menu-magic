e1a91c90a6ae1ac619bb897ec693dfa0c2f7c3ad
Ido Leybovitch <idoleybo01@gmail.com>
2025-07-10 20:44:57 +0300

    fix import img

--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 
 export const MenuHeader = () => {
   return (
-    // <header className="flex h-[300px] flex-col items-center justify-center py-8 px-4 bg-[url('src/assets/header_bg_img.jpeg')] bg-cover bg-center rounded-lg shadow-md mb-8">
         <header className="relative h-[300px] flex flex-col items-center justify-center px-4 py-8 rounded-lg shadow-md mb-8 overflow-hidden">
 
-        <div className="absolute inset-0 bg-[url('src/assets/header_bg_img.jpeg')] bg-cover bg-center opacity-80 z-0" ></div>
+        <img src={new URL('../assets/header_bg_img.jpeg', import.meta.url).href} className="absolute inset-0 opacity-80 z-0 w-full h-full object-cover" />
 
       <h1 className="text-4xl md:text-6xl font-sans font-bold text-white mb-2 z-10">קפה פאוזה</h1>
       <p className="text-xl text-cafe-cream font-medium z-10">תפריט</p>
